8e4dd9eb0235eef269ff0bd0bb2ad37f20c82e4e
Kaio Sumikawa <kaiosumikawa@gmail.com>
2026-01-05 11:11:43 +0900

    Aula Prática 10 – Herança (Parte 1)

--- src/projetopessoas/ProjetoPessoas.java
+++ src/projetopessoas/ProjetoPessoas.java
@@ -11,5 +11,22 @@ public class ProjetoPessoas {
         p2.setNome("Maria");
         p3.setNome("Cláudio");
         p4.setNome("Fabiana");
+
+        p1.setSexo("M");
+        p4.setSexo("F");
+        p2.setIdade(18);
+
+        p2.setCurso("Informática");
+        p3.setSalario(2500.75f);
+        p4.setSetor("Estoque");
+
+        // p1.receberAumento(550.20f);
+        // p2.mudarTrabalho();
+        // p4.cancelarMatr();
+
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+        System.out.println(p4.toString());
     }
 }
