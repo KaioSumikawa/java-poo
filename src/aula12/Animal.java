a169235dec04c243eeb97d0eccf0e2d4023fd7c1
Kaio Sumikawa <kaiosumikawa@gmail.com>
2026-01-07 08:57:59 +0900

    Aula Prática 12 – Polimorfismo em Java (Parte 1)

---
+++ src/aula12/Animal.java
@@ -0,0 +1,11 @@
+package aula12;
+public class Animal {
+    // Atributos de Animal
+    protected float peso;
+    protected int idade;
+    protected int membros;
+    // Métodos de Animal
+    public abstract void locomover();
+    public abstract void alimentar();
+    public abstract void emitirSom();
+}
