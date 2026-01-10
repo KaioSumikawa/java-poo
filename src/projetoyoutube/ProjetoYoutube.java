0bfce80c5f7250cdabb49fe655a874767ec7722d
Kaio Sumikawa <kaiosumikawa@gmail.com>
2026-01-11 06:23:29 +0900

    Aula Prática 15 – Projeto Final em Java (Parte 2)

--- src/projetoyoutube/ProjetoYoutube.java
+++ src/projetoyoutube/ProjetoYoutube.java
@@ -10,6 +10,15 @@ public class ProjetoYoutube {
         g[0] = new Gafanhoto("Jubileu", 22, "M", "juba");
         g[1] = new Gafanhoto("Creuza", 12, "F", "creuzita");
 
+        Visualizacao vis = new Visualizacao[5];
+        vis[0] = new Visualizacao(g[0], v[2]); // Jubileu assiste HTML5
+        vis[0].avaliar();
+        System.out.println(vis[0].toString());
+
+        vis[1] = new Visualizacao(g[0], v[1]); // Jubileu assiste PHP
+        vis[0].avaliar(87.0f);
+        System.out.println(vis[0].toString);
+
         /*System.out.println("VÍDEOS\n------------------");
         System.out.println(v[0].toString());
         System.out.println(v[1].toString());
--- src/projetoyoutube/Visualizacao.java
+++ src/projetoyoutube/Visualizacao.java
@@ -6,6 +6,30 @@ public class Visualizacao {
     public Visualizacao(Gafanhoto espectador, Video filme) {
         this.espectador = espectador;
         this.filme = filme;
+        this.espectador.setTotAssistido(this.espectador.getTotAssistido() + 1);
+        this.filme.setViews(this.filme.getViews() + 1);
+    }
+
+    public void avaliar() {
+        this.filme.setAvaliacao(5);
+    }
+
+    public void avaliar(int nota) {
+        this.filme.setAvaliacao(nota);
+    }
+
+    public void avaliar(float porc) {
+        int tot = 0;
+        if (porc <= 20) {
+            tot = 3;
+        } else if (porc <= 50) {
+            tot = 5;
+        } else if (porc <= 90) {
+            tot = 8;
+        } else {
+            tot = 10;
+        }
+        this.filme.setAvaliacao(tot);
     }
 
     public Gafanhoto getEspectador() {
@@ -22,5 +46,10 @@ public class Visualizacao {
 
     public void setFilme(Video filme) {
         this.filme = filme;
+    }
+
+    @Override
+    public String toString() {
+        return "Visualizacao{" + "espectador=" + espectador + ", filme=" + filme + '}';
     }
 }
